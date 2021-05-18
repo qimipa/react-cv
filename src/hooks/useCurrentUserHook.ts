5f3edf4bb43d84ae0c571f89f5b1d687fe3729e8
Pan <qimipa@outlook.com>
2021-05-17 21:20:22 -0500

    added logout

---
+++ src/hooks/useCurrentUserHook.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const useCurrentUserHook = () => {
+  const [user, setUser] = useState<firebase.User | null>(null);
+
+  useEffect(() => {
+    const auth = firebase.auth();
+
+    const unsubscribe = auth.onAuthStateChanged((e) => {
+      setUser(e);
+    });
+
+    return unsubscribe;
+  }, []);
+  return user;
+};
+
+export default useCurrentUserHook;
